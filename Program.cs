52351643c8d74f6b8d3b4223c0a7831e8787976a
bajonator <50272290+bajonator@users.noreply.github.com>
2023-04-16 22:49:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieDomowe4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Wpisz jakąś liczbę:");
+                var number = GetInputNumber();
+                Console.WriteLine(GetInformation(number));
+            }
+        }
+
+        private static string GetInformation(int number)
+        {
+            if (number % 2 == 0)
+                return "liczba parzysta";
+            else
+                return "Liczba nieparzysta";
+        }
+
+        private static int GetInputNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int result))
+                {
+                    Console.WriteLine("Podano nieprawidłową wartość.\nWprowadź nowa wartość liczbową");
+                    continue;
+                }
+                return result;
+            }
+        }
+    }
+}
